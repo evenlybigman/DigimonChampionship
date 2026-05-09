6d44bddafdbb96ee34500e3ac7d9de4a9106efb5
Eve (Juhyeok Kim) <juhyeok.works@gmail.com>
2026-05-09 16:00:29 +0900

    first commit

---
+++ js/data/digimon.js
@@ -0,0 +1,41 @@
+const DIGIMON_DATA = {
+    botamon: {
+        id: "botamon",
+        name: "Botamon",
+        stage: 0,
+        type: "free",
+        attribute: "none",
+        baseStats: {
+             hp:10,
+             mp:10,
+             atk:10,
+             def:10,
+             spd:10},
+    },
+     koromon: {
+        id: "koromon",
+        name: "Koromon",
+        stage: 1,
+        type: "free",
+        attribute: "none",
+        baseStats: {
+             hp:20,
+             mp:20,
+             atk:20,
+             def:20,
+             spd:20},
+    },
+    agumon: {
+        id: "agumon",
+        name: "Agumon",
+        stage: 2,
+        type: "free",
+        attribute: "none",
+        baseStats: {
+             hp:30,
+             mp:30,
+             atk:30,
+             def:30,
+             spd:30},
+    },
+};
